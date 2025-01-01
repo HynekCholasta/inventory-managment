5c77c746a9f914707ae5ab4a83fa37241acf5fed
HynekCholasta <hynacholasta@gmail.com>
2025-01-01 10:05:44 +0100

    added image urls in a s3 bucket

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "s3-inventorymanagement.s3.us-east-2.amazonaws.com",
+        hostname: "s3-inventory-management123123.s3.eu-west-1.amazonaws.com",
         port: "",
         pathname: "/**",
       },
